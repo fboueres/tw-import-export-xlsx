d42196583c55ef1d5d521012e4ce148fffd3456a
fernandoboueres <fernandoboueres.ti@gmail.com>
2023-05-10 11:24:57 -0300

    projeto do curso finalizado

---
+++ resources/views/admin/clients/excel.blade.php
@@ -0,0 +1,16 @@
+<table>
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($clients as $item)
+            <tr>
+                <td>{{ $item->nome }}</td>
+                <td>{{ $item->email }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
